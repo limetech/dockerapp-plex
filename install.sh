d283f9c0180eb2045f19aa320b772c5bb6730790
Eric Schultz <eric@startuperic.com>
2017-06-22 21:35:56 -0500

    update to Plex 1.5.7.4016-25d94bad9

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f /bin/true /usr/bin/chfn
 apt-get -q update
 apt-get install -qy gdebi-core wget
 
-# Plex version 1.5.6.3790-4613ce077
+# Plex version 1.5.7.4016-25d94bad9
 PLEX_URL=$(curl -sL https://plex.tv/api/downloads/1.json | sed -nr 's#.*"url":"([^"]+?\/plexmediaserver_[^"]+?_amd64\.deb)".*#\1#p')
 PLEX_VERSION=$(echo $PLEX_URL | awk -F_ '{print $2}')
 
