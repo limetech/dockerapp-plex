7e57d45a8e8ccdbf73d0fccb3fb6bbc3a1119fb7
Eric Schultz <eric@startuperic.com>
2017-10-12 06:15:30 -0500

    update to Plex 1.9.4.4325-1bf240a65

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f /bin/true /usr/bin/chfn
 apt-get -q update
 apt-get install -qy gdebi-core wget
 
-# Plex version 1.9.2.4285-9f65b88ae
+# Plex version 1.9.4.4325-1bf240a65
 PLEX_URL=$(curl -sL https://plex.tv/api/downloads/1.json | sed -nr 's#.*"url":"([^"]+?\/plexmediaserver_[^"]+?_amd64\.deb)".*#\1#p')
 PLEX_VERSION=$(echo $PLEX_URL | awk -F_ '{print $2}')
 
