ab0c6606f51dba8d7c52f46df8a385e6b7c3ede3
Eric Schultz <eric@startuperic.com>
2019-03-22 16:56:35 -0500

    update to Plex 1.15.2.793-782228f99

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f /bin/true /usr/bin/chfn
 apt-get -q update
 apt-get install -qy gdebi-core wget
 
-# Plex version 1.15.1.791-8bec0f76c
+# Plex version 1.15.2.793-782228f99
 PLEX_URL=$(curl -sL https://plex.tv/api/downloads/1.json | sed -nr 's#.*"url":"([^"]+?debian\/plexmediaserver_[^"]+?_amd64\.deb)".*#\1#p')
 PLEX_VERSION=$(echo $PLEX_URL | awk -F_ '{print $2}')
 
