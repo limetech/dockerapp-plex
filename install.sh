ee6f023c00f31e31c6982126071514e4984427bd
Eric Schultz <eric@startuperic.com>
2019-12-21 01:55:36 -0600

    Update to Plex 1.18.3.2156-349e9837e

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f /bin/true /usr/bin/chfn
 apt-get -q update
 apt-get install -qy gdebi-core wget
 
-# Plex version 1.18.2.2058-e67a4e892
+# Plex version 1.18.3.2156-349e9837e
 PLEX_URL=$(curl -sL https://plex.tv/api/downloads/1.json | sed -nr 's#.*"url":"([^"]+?debian\/plexmediaserver_[^"]+?_amd64\.deb)".*#\1#p')
 PLEX_VERSION=$(echo $PLEX_URL | awk -F_ '{print $2}')
 
